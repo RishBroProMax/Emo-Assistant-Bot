b7b446744f23cea9df07e966cd55fb70dbfe8d39
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-11-25 22:17:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+# Credits @ImDenuwan
+
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from news import LK
+
+
+API_ID = ''
+API_HASH = ''
+BOT_TOKEN = ''
+
+bot = Client(
+    "lol",
+    api_id=API_ID,
+    api_hash=API_HASH, 
+    bot_token=BOT_TOKEN,
+)
+
+@bot.on_message(filters.command("start"))
+async def start(_, m : Message):
+    await m.reply_text("✨Hello.. \n\n I m Simple And Fast News Bot \n Send /news For Get Sri Lankan News.\n\n Powerd By @EmoBotDevolopers \n Credits :- @ImRishmika | @Denuwan or @ImDenuwan")
+
+@bot.on_message(filters.command("news"))
+async def news(_, m : Message):
+    lol = await m.reply_text("Processing...")
+    nw = LK()
+    await lol.delete()
+    await m.reply_photo(nw[0]['img_url'], caption=f"**📰 {nw[0]['Title']}**\n\n✍️ {nw[0]['Description']}.__[See more...]({nw[0]['Link']})__\n\n📅 {nw[0]['Date']}\n\n🔰 Powered By 🔰 :- newswire.lk \n ❤️ </> Emo Bot Devolopers", reply_markup=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("📰 View in Site 📰", url=nw[0]['Link'])
+            ],
+            [
+                InlineKeyboardButton("</> Emo Bot Devolopers", url="t.me/EmoBotDevolopers")
+            ]
+        ]
+    ))
+
+print("Connecting On Emo Network...")
+print("Connected On Emo Network!")
+print("Checking Erorrs...")
+print("Geting News On newswire.lk")
+print("Saving On My Database..")
+print("Rishmika Sandanu's API Working Fine")
+print("Bot Stated. Check Now")
+bot.run()
