3468dfac1a5e1c88ad9a037e3d74e148a2a9c4ca
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-02 14:52:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ START = "👋Hello.. \n\n I m Assistant Bot Of Team Emo. \n Send /help For Get A
 
 # Start Massege
 
-@bot.on.message(filters.command("start"))
+@Client.on.message(filters.command("start"))
 async def start(bot, Message):
     await message.reply_photo("https://telegra.ph/file/79ee63a32d8e38842dfa0.jpg",captioin=start,reply_markup=InlineKeyboardMarkup([
         [
@@ -34,26 +34,26 @@ async def start(bot, Message):
 
 # Help Massege
 
-@bot.on_message(filters.command("help"))
+@Client.on_message(filters.command("help"))
 async def start(_, m : Message):
     await m.reply_text("🧩Commands \n\n /start - Start Message \n\n /help - Help Message \n\n /masseges - Get Bot Information \n /bots - More Bots \n /about  - About Massege")
 
 # About MAssege
 
-@bot.on_message(filters.command("about"))
+@Client.on_message(filters.command("about"))
 async def start(_, m : Message):
     await m.reply_text("✨ About Me \n\n 🧑‍💻Devoloper - @ImRishmika \n\n 📡API - ImRishmika API \n\n 💻Host Sever - Raliway")
 
 # More Bots
 
-@bot.on_message(filters.command("bots"))
+@Client.on_message(filters.command("bots"))
 async def start(_, m : Message):
     await m.reply_text("🧩BOTS Stetus \n\n @emFsub_Bot ❌ \n\n @emURLBypasser ❌ \n\n @EmoAssistant_Bot ✅ \n\n @emAFK_Bot ❌ \n\n @MRCM_Scouts_Bot ✅ \n\n @ImRishmika_Bot ✅ \n\n @Katszuki_Bot  ❌  \n\n 3 Bots Are Running.. \n 4 Bots Are Downing")
 
 
 
 
-@bot.on_message(filters.command("masseges"))
+@Client.on_message(filters.command("masseges"))
 async def start(_, m : Message):
     await m.reply_text("🔒Only Admin Can Use This Command")
 
