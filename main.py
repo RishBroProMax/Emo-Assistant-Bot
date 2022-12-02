a1e47f4964498819a39beca1245c3809c023f788
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-02 21:33:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ START = """
 HELP = """
 **All Commands**
 
-/start
-/help
-/bots
+/start - Start Message
+/help - Help Message
+/bots - Bot Stetus Cracker
 """
 
 BOTS = """
-♻️ Bot Stetus Chacker ♻️ 
+**♻️ Bot Stetus Chacker ♻️**
 
 ✅ = Running..  
 ❌ = Not Working.. 
@@ -54,16 +54,16 @@ BOTS = """
 # Commands
 @app.on_message(filters.command("start"))
 async def start(bot, message):
-  await message.reply_photo("https://telegra.ph/file/79ee63a32d8e38842dfa0.jpg",caption=START,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="</> ємσ вσт ∂єνσℓσρєʀѕ", url="t.me/EmoBotDevolopers"), InlineKeyboardButton(text="💠Github💠", url="https://github.com/RishBroProMax")],[InlineKeyboardButton(text="☘️Devoloper☘️", url="https://t.me/ImRishmika")]]))
+  await message.reply_photo("https://telegra.ph/file/c4ea3761bb73bab726334.jpg",caption=START,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="</> ємσ вσт ∂єνσℓσρєʀѕ", url="t.me/EmoBotDevolopers"), InlineKeyboardButton(text="💠Github💠", url="https://github.com/RishBroProMax")],[InlineKeyboardButton(text="☘️Devoloper☘️", url="https://t.me/ImRishmika")]]))
 
 @app.on_message(filters.command("help"))
 async def help(bot, message):
-  await message.reply_photo("https://telegra.ph/file/79ee63a32d8e38842dfa0.jpg",caption=HELP,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="</> ємσ вσт ∂єνσℓσρєʀѕ", url="t.me/EmoBotDevolopers")]]))
+  await message.reply_photo("https://telegra.ph/file/c4ea3761bb73bab726334.jpg",caption=HELP,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="</> ємσ вσт ∂єνσℓσρєʀѕ", url="t.me/EmoBotDevolopers")]]))
 
 @app.on_message(filters.command("bots"))
 async def bots(bot, message):
   await message.reply_photo("https://telegra.ph/file/c4ea3761bb73bab726334.jpg",caption=BOTS,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="</> ємσ вσт ∂єνσℓσρєʀѕ", url="t.me/EmoBotDevolopers")]]))
 
     
-print("Bot Stated. Check Now")
+print("Bot Stated.)
 app.run()
