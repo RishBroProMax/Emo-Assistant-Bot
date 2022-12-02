9171a522475eb697e30ab63341e6acf4a1e0ed79
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-02 15:29:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
 
+import pyrogram
+from pyrogram import filters, idle
+from pyrogram.errors import FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
+
+API_ID = '14055090'
+API_HASH = 'a46f7b439d0afa45b7a69fc450f754e9'
+BOT_TOKEN = '5715378157:AAF9kdjlzO6zaEvQHVDIunKOYce3PzFQH6c'
+
+app = Client(
+  "bot",
+  api_id=API_ID,
+  api_hash=API_HASH,
+  bot_token=BOT_TOKEN
+)
+
+START = """
+👋Hello.. \n\n I m Assistant Bot Of Rishmika Sandanu. \n Send /help For Get All Commands.\n\n 🔰Powerd By @EmoBotDevolopers \n ❤️Credits :- @ImRishmika
+"""
+
+HELP = """
+**All Commands**
+
+/start
+/help
+/bots
+/test 
+"""
+
+# Commands
+@app.on_message(filters.command("start"))
+async def start(bot, message):
+  await message.reply_photo("https://telegra.ph/file/79ee63a32d8e38842dfa0.jpg",caption=START,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="</> ємσ вσт ∂єνσℓσρєʀѕ", url="t.me/EmoBotDevolopers"), InlineKeyboardButton(text="💠Github💠", url="https://github.com/RishBroProMax")],[InlineKeyboardButton(text="☘️Devoloper☘️", url="https://t.me/ImRishmika")]]))
+
+@app.on_message(filters.command("help"))
+async def help(bot, message):
+  await message.reply_photo("https://telegra.ph/file/79ee63a32d8e38842dfa0.jpg",caption=HELP,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="</> ємσ вσт ∂єνσℓσρєʀѕ", url="t.me/EmoBotDevolopers")]]))
+
+@app.on.message(filters.command("bots"))
+async def help(_, m : message):
+    awaitm.reply_txt("♻️ Bot Stetus Chacker ♻️ \n ✅ = Running.. \n ❌ = Not Working.. \n\n  @emFsub_Bot ❌\n\n @emURLBypasser_Bot ❌\n\n @MRCM_Scouts_Bot ✅ \n\n @emAFK_Bot ❌\n\n @Katszuki_Bot ❌\n\n @ImRishmika_Bot ✅")
+
+@app.on.message(filters.command("bots"))
+async def help(_, m : message):
+    awaitm.reply_txt("❤️ I m Alive Now...")
