074a699b750907202992f11d74d2160af3c3eac8
Dilum125 <117908721+Dilum125@users.noreply.github.com>
2022-12-07 19:05:37 +0530

    Update addusers.py

--- Bot/database/addusers.py
+++ Bot/database/addusers.py
@@ -16,33 +16,9 @@ from Userbot.helpers.database import (
 async def AddUserToDatabase(bot: Client, cmd: Message):
     if not await is_served_user(cmd.from_user.id):
         await add_served_user(cmd.from_user.id)
-        if Config.LOG_GRP is not None:
-            await bot.send_message(
-                Config.LOG_GRP,
-                f"""
-**#NEW_USER**
 
-📛 **NAME**   :[{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})
-👤 **USER ID**:`{cmd.from_user.id}`
-
-**[{Config.BOT_NAME} ✅](https://t.me/{Config.BOT_USERNAME})**
-""",
-                disable_web_page_preview=True
-                )
 
 async def AddChatToDatabase(bot: Client, cmd: Message):
     if not await is_served_chat(cmd.chat.id):
         await add_served_chat(cmd.chat.id)
-        if Config.LOG_GRP is not None:
-            await bot.send_message(
-                Config.LOG_GRP,
-                f"""
-**#NEW_CHAT**
 
-📛 **CHAT NAME**:[Link](tg://user?id={cmd.chat.id})
-👤 **CHAT ID**  :`{cmd.chat.id}`
-
-**[{Config.BOT_NAME} ✅](https://t.me/{Config.BOT_USERNAME})**
-""",
-                disable_web_page_preview=True
-            )
