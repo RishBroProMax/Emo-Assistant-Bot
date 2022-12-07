a5488d6c5d5d825d886eecaa06957948e2d1d68f
Dilum125 <117908721+Dilum125@users.noreply.github.com>
2022-12-07 19:58:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-from pyrogram import Client, filters
+import os
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 
-API_ID = '14055090'
-API_HASH = 'a46f7b439d0afa45b7a69fc450f754e9'
-BOT_TOKEN = '5715378157:AAF9kdjlzO6zaEvQHVDIunKOYce3PzFQH6c'
-
-app = Client(
-  "bot",
-  api_id=API_ID,
-  api_hash=API_HASH,
-  bot_token=BOT_TOKEN
-)
 
 START = """
 👋Hello.. \n\n I m Assistant Bot Of Rishmika Sandanu. \n Send /help For Get All Commands.\n\n 🔰Powerd By @EmoBotDevolopers \n ❤️Credits :- @ImRishmika
@@ -51,21 +41,3 @@ BOTS = """
 
 """
 
-
-
-
-# Commands
-@app.on_message(filters.command("start"))
-async def start(bot, message):
-  await message.reply_photo("https://telegra.ph/file/c4ea3761bb73bab726334.jpg",caption=START,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="</> ємσ вσт ∂єνσℓσρєʀѕ", url="t.me/EmoBotDevolopers"), InlineKeyboardButton(text="💠Github💠", url="https://github.com/RishBroProMax")],[InlineKeyboardButton(text="☘️Devoloper☘️", url="https://t.me/ImRishmika")]]))
-
-@app.on_message(filters.command("help"))
-async def help(bot, message):
-  await message.reply_photo("https://telegra.ph/file/c4ea3761bb73bab726334.jpg",caption=HELP,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="</> ємσ вσт ∂єνσℓσρєʀѕ", url="t.me/EmoBotDevolopers")]]))
-
-@app.on_message(filters.command("bots"))
-async def bots(bot, message):
-  await message.reply_photo("https://telegra.ph/file/c4ea3761bb73bab726334.jpg",caption=BOTS,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="</> ємσ вσт ∂єνσℓσρєʀѕ", url="t.me/EmoBotDevolopers")]]))
-
-  print("Bot Stated")
-app.run()
