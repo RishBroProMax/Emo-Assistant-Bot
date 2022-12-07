267d58e747e73e7d352b3f08d15d484fd6081133
Dilum125 <117908721+Dilum125@users.noreply.github.com>
2022-12-07 19:21:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from os import environ
 from os import getenv
 
 class Config(object):
-	APP_ID = int(14055090)
-	API_HASH = "a46f7b439d0afa45b7a69fc450f754e9"
-	BOT_TOKEN = "5715378157:AAF9kdjlzO6zaEvQHVDIunKOYce3PzFQH6c"
-	MONGODB_URI = ""
+     APP_ID = int(14055090)
+     API_HASH = "a46f7b439d0afa45b7a69fc450f754e9"
+     BOT_TOKEN = "5715378157:AAF9kdjlzO6zaEvQHVDIunKOYce3PzFQH6c"
+     MONGODB_URI = ""
+     OWNER = int(5262156299)
