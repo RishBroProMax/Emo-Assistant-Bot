55dd2c9c69a17112fe52247ffb2db1c748340158
Dilum125 <117908721+Dilum125@users.noreply.github.com>
2022-12-07 18:14:06 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+from os import environ
+from os import getenv
+
+class Config(object):
+	APP_ID = int(9033504)
+	API_HASH = "d172967aa5b8834153a96501b588befe"
+	BOT_TOKEN = "5747689559:AAGrNOV-CkOHbOCGFYavZUfTp5yK1ZDLOFA"
+	SESSION = "BQCJ1yAAfOQRbv1LzNlbav7t-46n8rr3xaE5Or5HKTo04vYF5VWefU2T4F45TuBVD-jaq0QlqgmYA7tvxnUWj6gBo5z_kcmO7TyuEK_g4r9sa9svxPgLWUep14Ak3IV_bbgArMB1SHNhGSBFVc5C4M_Tn9CwU1hhuzSDyLpq60swGRFEni0irLTz0s155Ja5Mq1MQakGb49JJWVJJPH4HMiNLyfv6vRGseFNgivwX50e3WY_oGFOKViAXBhUSw2m5SWAJIUsYqFJHFwmwV6kK4WwPyqn0IJk7eIn6UZ9T143r2GLHQLlvx--Q5cnIcroDyWT7R5PHLAWhVN6QzD2ukDnllqGpwAAAAE5piILAA"
+	BOT_USERNAME = "DilumBBandara_003_bot"
+	BOT_NAME = "DɪʟᴜᴍBBᴀɴᴅᴀʀᴀ [𝔹𝕝𝕒𝕔𝕜ℍ𝕒𝕥]° [ᴄɢꜱ™] [ᴍᴀʀs™]"
+	OWNER = int(5262156299)
+	LOG_GRP = int(-1001766852309)
+	START_STICKER = "CAACAgIAAxkBAAEPHLJjghR6vFDhn2xpXLhvGRshEzpPeQACbwAD29t-AAGZW1Coe5OAdCsE"
+	CHANNEL = "Team_Mars_11"
+	CHANNEL_ID = int(-1001503113174)
+	GROUP = "TeamMars_11"
+	START_PIC = "https://graph.org/file/b31317f8d65885ddb7fc1.jpg"
+	C_CHANNEL = "About_xyz"
+	MONGODB_URI = "mongodb+srv://Userbot:dilum14483@userbot.hngsqcc.mongodb.net/?retryWrites=true&w=majority"
+	ARQ_API_KEY = "TLKZEM-FSYGYW-OTINGB-RIMVDM-ARQ"
