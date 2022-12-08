9df5575ba55deb32b948cc24c9bd9de0bab4e17c
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-08 05:46:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
      APP_ID = int(14055090)
      API_HASH = "a46f7b439d0afa45b7a69fc450f754e9"
      BOT_TOKEN = "5715378157:AAF9kdjlzO6zaEvQHVDIunKOYce3PzFQH6c"
-     MONGODB_URI = ""
-     OWNER = int(5262156299)
+     MONGODB_URI = "mongodb+srv://semmy:semmyvcbot@cluster0.imrqu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+     OWNER = int(5171347305)
      BOT_USERNAME = "EmoAssistant_Bot"
